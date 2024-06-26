20961d90a4c25cf460933f2ecd52731e89843dbc
MRsy22* <sayeghomar52@gmail.com>
2024-06-26 18:16:36 +0100

    update checkpoint

---
+++ src/component/Name.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import product from './product'
+const Name = () => {
+  return (
+    <div>
+      <h2>{product.name} </h2>
+    </div>
+  )
+}
+
+export default Name
---
+++ src/component/product.js
@@ -0,0 +1,7 @@
+export const product ={
+    name:"BMW",
+    price:"167.900$",
+    description:"Type de motorisation. Essence - Hybridation Légère (48V) ; Puissance ch (kW). 170 (125) ;",
+    URlimage:"https://www.bmw.fr/content/dam/bmw/common/all-models/1-series/5-door/2024/navigation/bmw-1-series-modelfinder.png",
+};
+export default product;
